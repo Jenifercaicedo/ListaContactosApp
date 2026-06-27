f36d41da12ea945b7db8bde1bdd7e4a74e103670
Jenifercaicedo <caicedojenifer94@gmail.com>
2026-06-27 17:17:28 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import ContactList from './components/ContactList';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ContactList />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EAF7EA',
+  },
+});
